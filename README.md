4177e30105898018a5848a25c7cd65dc27daf621
Maksim Korzh <freesoft.for.people@gmail.com>
2021-03-04 14:09:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# ccbridge-js
+# CCBridge JS (under development)
 Cross-platform Chinese chess xiangqi GUI inspired by CCBridge
+
+# Run web app
+coming soon...
+
+# Run desktop app
+```bash
+# Clone this repository
+git clone https://github.com/electron/electron-quick-start
+# Go into the repository
+cd electron-quick-start
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
---
+++ main.js
@@ -0,0 +1,114 @@
+// import packages
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+// set environment
+process.env.NODE_ENV = 'development';
+//process.env.NODE_ENV = 'production';
+
+// app
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+// main window
+let mainWindow;
+
+// Listen for app ready
+app.on('ready', function() {
+  // create new window
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  
+  // load URL into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  
+  // quit app when closed
+  mainWindow.on('closed', function() {
+    app.quit();
+  });
+  
+  // build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// catch item:add
+ipcMain.on('item:add', function(e, item){
+  mainWindow.webContents.send('item:add', item);
+  testWindow.close(); 
+  // Still have a reference to addWindow in memory. Need to reclaim memory (Grabage collection)
+  testWindow = null;
+});
+
+function createTestWindow() {
+  // create new window
+  testWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    title: 'test window',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  
+  // load URL into window
+  testWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'testWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  
+  // garbage collection handle
+  testWindow.on('close', function() {
+    testWindow = null;
+  });
+}
+
+// create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New window',
+        click() {
+          createTestWindow();
+        }
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// add developer tools to menu if not in production
+if (process.env.NODE_ENV != 'production') {
+  mainMenuTemplate.push({
+    accelerator: 'Ctrl+Shift+I',
+    label: 'DevTools',
+    click(item, focusedWindow) {
+      focusedWindow.toggleDevTools();
+    }
+  },
+  {
+    role: 'reload'
+  });
+}
+
+
+
