8f758ec1b0753ee0667aa8a23b12ecb886999892
Maksim Korzh <freesoft.for.people@gmail.com>
2021-03-05 20:29:21 +0200

    forwarded editing features

--- scripts/mainWindow.js
+++ scripts/mainWindow.js
@@ -6,13 +6,60 @@
  ============================              
 \****************************/
 
+// highlight move in move list
+function highlightMove(move) {
+  // reset highlighting
+  document.getElementById('movelist').childNodes.forEach((item, index) => {
+    try {
+      if (index > 1) {
+        item.classList.remove('bg-primary');
+        item.classList.remove('text-white');
+      }
+    } catch(e) {}
+  });
+
+  // highlight move
+  highlight = document.getElementById('UBB_move_' + move);
+  highlight.classList.add('bg-primary');
+  highlight.classList.add('text-white');
+}
+
+// highlight variation
+function highlightVariation(variation) {
+  // reset highlighting
+  document.getElementById('variationlist').childNodes.forEach((item, index) => {
+    try {
+      if (index > 1) {
+        item.classList.remove('bg-primary');
+        item.classList.remove('text-white');
+      }
+    } catch(e) {}
+  });
+
+  // highlight move
+  highlight = document.getElementById(variation);
+  highlight.classList.add('bg-primary');
+  highlight.classList.add('text-white');
+}
+
 // update board
-function updateBoard(moveNumber) {
-  // update UBB board
-  gotonum(moveNumber);
+function updateGUIboard(moveNumber, variationId) {
+  if (moveNumber) {
+    // update UBB board
+    gotonum(moveNumber);
+  } else {
+    // parse agrs
+    let args = variationId.toString().split('get_movetext(')[1].split(')')[0].split(',');
+    
+    // update UBB board
+    get_movetext(args[0].split("'").join(''), args[1].split("'").join(''));
+  }
   
-  // get current move
+  // get moves
   let currentMove = document.getElementById('shownow').value.split('/')[0];
+  let lastMove = document.getElementById('shownow').value.split('/')[1];
+  
+  // get current FEN
   let UBBfen = getFENTEXT(P[currentMove]);
   
   // update engine board
@@ -21,31 +68,47 @@ function updateBoard(moveNumber) {
   // update GUI board
   board.position(UBBfen);
   
-  /*if (engine.getPiece(targetSquare) && moveStack.count > 0 && moveStack.count < moveStack.moves.length - 1) {
-    document.getElementById(targetSquare).style.backgroundColor = SELECT_COLOR;
-    playSound(move);
-  }*/
+  // current move to highlight
+  updateGameTree();
+  
+  // play sound
+  MOVE_SOUND.play()
 }
 
-// show first move of the game
-function firstMove() {
-  updateBoard('First');
+// edit comment
+function editComment() {
+  document.getElementById('c_text').value = document.getElementById('comments').value;
+  EditComment(cText);
 }
 
-// take move back
-function previousMove() {
-  updateBoard('Prev');
+// delete move/moves/variation
+function deleteMove() {
+  // delete on UBB board
+  DelMove();
+  clickYES();
+  
+  // click 'Yes' on delete error message
+  setTimeout(() => {
+    clickYES();
+  }, 100);
+
+  // get moves
+  let currentMove = document.getElementById('shownow').value.split('/')[0];
+  let lastMove = document.getElementById('shownow').value.split('/')[1];
+  
+  // get current FEN
+  let UBBfen = getFENTEXT(P[currentMove]);
+  
+  // update engine board
+  engine.setBoard(UBBfen);
+  
+  // update GUI board
+  board.position(UBBfen);
+  
+  // current move to highlight
+  updateGameTree();
 }
 
-// make next move
-function nextMove() {
-  updateBoard('Next');
-}
-
-// show last move of the game
-function lastMove() {
-  updateBoard('Last');
-}
 
 /****************************\
  ============================
@@ -55,17 +118,61 @@ function lastMove() {
  ============================              
 \****************************/
 
-// add move to move list view
-function addMove() {
-  let moveList = document.getElementById('movelist')
-  let moveItem = document.createElement('li');
-  let UBBMove = document.getElementById('move_' + engine.getPly()).innerText;
-  moveItem.id = 'UBB_move_' + engine.getPly();
-  moveItem.classList = 'btn list-group-item list-group-item-action m-0 p-0';
-  moveItem.style = 'font-size: 16px';
-  moveItem.textContent = UBBMove;
-  moveItem.setAttribute('onclick', 'updateBoard(parseInt(this.id.split("move_")[1]))');
-  moveList.appendChild(moveItem);
+// add variation
+function updateGameTree() {
+  // reset move list
+  let moveList = document.getElementById('movelist');
+  moveList.innerHTML = `
+    <li class="btn list-group-item bg-light m-0 p-0" style="font-size: 16px; font-weight: bold;">Move list</li>
+    <li id="UBB_move_0" class="btn list-group-item list-group-item-action text-center m-0 p-0" onclick="updateGUIboard('First')">Start</li>
+  `;
+  
+  // update move list
+  document.getElementById('m_text').childNodes.forEach((item) => {
+    if (item.tagName == 'DIV') {
+      let moveNumber = parseInt(item.id.split('_')[1]);
+      
+      if (moveNumber) {
+        let moveText = item.childNodes[1].parentNode.innerText;
+        let moveItem = document.createElement('li');
+        
+        moveItem.id = 'UBB_move_' + moveNumber;
+        moveItem.classList = 'btn list-group-item list-group-item-action m-0 p-0';
+        moveItem.style = 'font-size: 16px';
+        moveItem.textContent = moveText;
+        moveItem.setAttribute('onclick', 'updateGUIboard(parseInt(this.id.split("move_")[1]))');
+        moveList.appendChild(moveItem);
+      }
+      
+      if (item.style.backgroundColor == 'rgb(49, 106, 197)') highlightMove(moveNumber);
+    }
+  });
+  
+  // reset variation list
+  let variationList = document.getElementById('variationlist');
+  variationList.innerHTML = '<li class="btn list-group-item bg-light m-0 p-0" style="font-size: 16px; font-weight: bold;">Variations</li>';
+  
+  // update variation list
+  document.getElementById('v_text').childNodes.forEach((item) => {
+    if (item.tagName == 'DIV') {
+      let variationNumber = parseInt(item.id.split('_')[1]);
+      let variationText = item.childNodes[1].innerText;
+      let variationItem = document.createElement('li');
+      let clickVariation = item.onclick.toString();
+      
+      variationItem.id = 'UBB_variation(' + item.id + ')';
+      variationItem.classList = 'btn list-group-item list-group-item-action m-0 p-0';
+      variationItem.style = 'font-size: 16px';
+      variationItem.textContent = variationText;
+      variationItem.setAttribute('onclick', 'updateGUIboard(0, ' + clickVariation + ');');
+      variationList.appendChild(variationItem);
+      
+      if (item.style.backgroundColor == 'rgb(49, 106, 197)') highlightVariation(variationItem.id);
+    }
+  });
+  
+  // update comments
+  document.getElementById('comments').value = document.getElementById('c_text').value.trim();
 }
 
 // update DPXQ board
@@ -81,8 +188,13 @@ function updateUBB(source, target) {
   getMove(ubbSource);
   getMove(ubbTarget);
   
-  // add move to UBB move list
-  addMove();
+  // add variation (optional)
+  if (document.getElementById('v_addnew').childNodes.length)
+    if (document.getElementById('v_addnew').scrollWidth)
+      document.getElementById('v_addnew').querySelector('a').click();
+  
+  // add move to move list
+  setTimeout(() => {updateGameTree();}, 0);
 }
 
 
@@ -118,12 +230,32 @@ function onDrop (source, target) {
   
   // sync with DPXQ board
   updateUBB(source, target);
+  
+  // play sound
+  if (validMove) playSound(validMove);
 }
 
 // update the board position after the piece snap
-// for castling, en passant, pawn promotion
 function onSnapEnd () {
   board.position(engine.generateFen());
+}
+
+/****************************\
+ ============================
+ 
+         BOARD SOUNDS
+
+ ============================              
+\****************************/
+
+// import sounds
+const MOVE_SOUND = new Audio('../libs/xiangqiboardjs-0.3.3/sounds/move.wav');
+const CAPTURE_SOUND = new Audio('../libs/xiangqiboardjs-0.3.3/sounds/capture.wav');
+
+// play sound
+function playSound(move) {
+  if (engine.getCaptureFlag(move)) CAPTURE_SOUND.play();
+  else MOVE_SOUND.play();
 }
 
 
